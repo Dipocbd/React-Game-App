d07906195cff601cad2840e18452fad2e858fb11
Dipocbd <dipoojo6@gmail.com>
2023-01-06 12:47:56 -0800

    Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+     <div className="main">main</div>
+     <div className="pyramid">
+      <ul className="moneyList">
+        <li className="moneyListItem active">
+          <span className="moneyListItemNumber">4</span>
+          <span className="moneyListItemAmount">₦500</span>
+          </li>
+          <li className="moneyListItem">
+          <span className="moneyListItemNumber">4</span>
+          <span className="moneyListItemAmount">₦500</span>
+          </li>
+          <li className="moneyListItem">
+          <span className="moneyListItemNumber">4</span>
+          <span className="moneyListItemAmount">₦500</span>
+          </li>
+          <li className="moneyListItem">
+          <span className="moneyListItemNumber">4</span>
+          <span className="moneyListItemAmount">₦500</span>
+          </li>
+        
+      </ul>
+     </div>
     </div>
   );
 }
---
+++ src/Data.js
@@ -0,0 +1,61 @@
+export const moneyPyramid = [
+    {
+        id:1,
+        amount:"₦500"
+    },
+
+    {
+        id:2,
+        amount:"₦1,000"
+    },
+    
+    {
+        id:3,
+        amount:"₦2,000"
+    },
+
+    {
+        id:3,
+        amount:"₦4,000"
+    },
+
+    {
+        id:4,
+        amount:"₦8,000"
+    },
+
+    {
+        id:5,
+        amount:"₦16,000"
+    },
+
+    {
+        id:6,
+        amount:"₦32,000"
+    },
+
+    {
+        id:7,
+        amount:"₦64,000"
+    },
+
+    {
+        id:8,
+        amount:"₦128,000"
+    },
+
+    {
+        id:9,
+        amount:"₦256,000"
+    },
+
+    {
+        id:10,
+        amount:"₦512,000"
+    },
+
+    {
+        id:11,
+        amount:"₦1,024,000"
+    },
+]
