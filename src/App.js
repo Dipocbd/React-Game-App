9f416e115d9a51a87058e70412557cedddaa9f00
Dipocbd <dipoojo6@gmail.com>
2023-01-06 14:41:37 -0800

    Created and array in a js file and added some contents

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
+import { useState } from "react";
 import "./index.css"
+import { moneyPyramid } from "./Data";
 
 function App() {
+  const [first, setfirst] = useState(5)
   return (
     <div className="app">
-     <div className="main">main</div>
+     <div className="main">
+    <div className="top">
+      <div className="timer">30</div>
+    </div>
+    <div className="bottom">Questions and Answers</div>
+
+     </div>
      <div className="pyramid">
       <ul className="moneyList">
-        <li className="moneyListItem active">
-          <span className="moneyListItemNumber">4</span>
-          <span className="moneyListItemAmount">₦500</span>
+      {
+        moneyPyramid.map(m=>(
+          <li className={first === m.id ? "moneyListItem active" : "moneyListItem"}>
+          <span className="moneyListItemNumber">{m.id}</span>
+          <span className="moneyListItemAmount">{m.amount}</span>
           </li>
-          <li className="moneyListItem">
-          <span className="moneyListItemNumber">4</span>
-          <span className="moneyListItemAmount">₦500</span>
-          </li>
-          <li className="moneyListItem">
-          <span className="moneyListItemNumber">4</span>
-          <span className="moneyListItemAmount">₦500</span>
-          </li>
-          <li className="moneyListItem">
-          <span className="moneyListItemNumber">4</span>
-          <span className="moneyListItemAmount">₦500</span>
-          </li>
+        ))
+      }
         
       </ul>
      </div>
--- src/Data.js
+++ src/Data.js
@@ -15,47 +15,47 @@ export const moneyPyramid = [
     },
 
     {
-        id:3,
+        id:4,
         amount:"₦4,000"
     },
 
     {
-        id:4,
+        id:5,
         amount:"₦8,000"
     },
 
     {
-        id:5,
+        id:6,
         amount:"₦16,000"
     },
 
     {
-        id:6,
+        id:7,
         amount:"₦32,000"
     },
 
     {
-        id:7,
+        id:8,
         amount:"₦64,000"
     },
 
     {
-        id:8,
+        id:9,
         amount:"₦128,000"
     },
 
     {
-        id:9,
+        id:10,
         amount:"₦256,000"
     },
 
     {
-        id:10,
+        id:11,
         amount:"₦512,000"
     },
 
     {
-        id:11,
+        id:12,
         amount:"₦1,024,000"
     },
-]
+].reverse()
